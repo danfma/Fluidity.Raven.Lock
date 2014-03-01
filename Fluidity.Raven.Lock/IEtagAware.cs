074da5994128db9ed1ec0887c8952786442d5e14
danfma <danfma@gmail.com>
2014-03-01 16:55:31 -0300

    RavenDB Updated

--- Fluidity.Raven.Lock/IEtagAware.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Fluidity.Raven.Lock
-{
-	public interface IEtagAware
-	{
-		Guid Etag { get; set; }
-	}
-}
--- Fluidity.Raven.Lock/ILocker.cs
+++ Fluidity.Raven.Lock/ILocker.cs
@@ -2,10 +2,13 @@ using System;
 
 namespace Fluidity.Raven.Lock
 {
+	/// <summary>
+	/// A locker instance
+	/// </summary>
 	public interface ILocker : IDisposable
 	{
 		/// <summary>
-		/// Estende o tempo de vido do lock, para garantir que a tarefa seja executada.
+		/// Extends the lifetime of the locker by the specified <paramref name="lifetime"/> from <c>now</c>.
 		/// </summary>
 		/// <param name="lifetime">The lifetime.</param>
 		void Renew(TimeSpan lifetime);
